19d553e33a174449d6a91dea12583cc82b82696d
Oyster <oyster@example.com>
2024-03-15 11:12:29 +0530

    jc

--- _posts/pro-notes/python.py
+++ _posts/pro-notes/python.py
@@ -1,3 +1,10 @@
-str1= 'Text enclosed with single quotes.'
-newStr = str1.replace("enclosed with", "and")
-print(str1[:7])
+matrix = [
+    [1,2, 3],
+    [4, 5, 6],
+    [7, 8, 9]
+]
+transpose = [
+    [row[colIndex] for row in matrix] 
+    for colIndex in range(len(matrix))
+]
+print(transpose)
